998f26c97ae050a590a1c2f261c5cad2a140312f
arya-satwika <aryastwk95@gmail.com>
2026-08-14 16:55:04 +0700

    fixed about

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -22,12 +22,11 @@ function About() {
                 {/* <h1 className="text-2xl font-bold">Hi, im 
                     <span className="text-main-red font-mono"> Arya Satwika</span>
                 </h1> */}
-                <p className="">
-                    Namaku Arya Satwika, mahasiswa Teknik Informatika UNESA angkatan 2024
-                    <br />Cinta teknologi, coding, computer, dan thinkering.
-                    Hobi bermain game, mendengarkan musik, dan editing / designing.
-                    Sedang mendalami dunia web development seperti React, dan Tanstack,
-                    ingin menjadi Fullstack Web Developer dan Systems Developer di masa depan.
+                <p className="tracking-wide">
+                    My name's Arya Satwika, i love thinkering, tech and understanding how stuff works
+                    I also like to learn usually by doing projects or reading up on it.
+                    Currently trying to get better at making useful programs and not just CRUD webapps.
+                    I aspire to be a great engineer and a creative person
                 </p>
                 </div>
                 {/* <Image src="/headshot.webp" 
@@ -37,15 +36,15 @@ function About() {
                 className="w-[200px] h-[200px] ml-2"/> */}
             </div>
             <div className='mt-10 text-lg'>
-                <div className='flex flex-row'><span className='text-clickable font-normal shrink-0 w-[8ch]'>
+                <div className='flex flex-row font-bold tracking-widest'><span className='text-clickable font-normal tracking-normal shrink-0 w-[8ch]'>
                     Systems</span><span className=' text-clickable mr-2'>\</span>
                     C++, Typescript, Python
                 </div>
-                <div className='flex flex-row'><span className='text-clickable font-normal shrink-0 w-[8ch]'>
+                <div className='flex flex-row font-bold tracking-widest'><span className='text-clickable font-normal tracking-normal shrink-0 w-[8ch]'>
                     Frontend</span><span className='text-clickable mr-2'>\</span>
                     Svelte, React, Tailwind, customtkinter,HTML, CSS, QML / quickshell
                 </div>
-                <div className='flex flex-row'><span className='text-clickable font-normal shrink-0 w-[8ch]'>
+                <div className='flex flex-row font-bold tracking-widest'><span className='text-clickable font-normal tracking-normal shrink-0 w-[8ch]'>
                     Tools</span><span className='   text-clickable mr-2'>\</span>
                     Tanstack, NextJS, Midtrans, Drizzle, uploadthing, Figma, After Effects, Davinci Resolve
                 </div>
